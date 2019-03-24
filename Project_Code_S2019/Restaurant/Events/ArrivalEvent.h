9e5160af01f6ae8e6f7070db8df463c283d426b2
Abdallah Hemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-03-24 23:37:36 +0200

    Update ArrivalEvent.h

--- Project_Code_S2019/Restaurant/Events/ArrivalEvent.h
+++ Project_Code_S2019/Restaurant/Events/ArrivalEvent.h
@@ -7,14 +7,15 @@
 //class for the arrival event
 class ArrivalEvent: public Event
 {
-	//info about the order ralted to arrival event
+	//Info about the order ralted to arrival event
 	int OrdDistance;	//order distance
-	ORD_TYPE OrdType;		//order type: Normal, Frozen, VIP
-	REGION OrdRegion;  //Region of this order	                
+	ORD_TYPE OrdType;	//order type: Normal, Frozen, VIP
+	REGION OrdRegion;	//Region of this order	                
 	double OrdMoney;	//Total order money
 public:
 	ArrivalEvent(int eTime, int oID, ORD_TYPE oType, REGION reg);
-	//Add more constructors if needed
+	ArrivalEvent();
+	
 	
 	virtual void Execute(Restaurant *pRest);	//override execute function
 
